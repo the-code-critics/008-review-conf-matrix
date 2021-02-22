b7a3fc87d6ddede0a1246da5348aa8e0283b12bc
Stefan Maetschke <stefan.maetschke@gmail.com>
2021-02-22 17:18:56 +1100

    code examples added

---
+++ original_code.py
@@ -0,0 +1,71 @@
+"""
+Original code to compute the elements of a confusion matrix
+https://github.com/abhishekkrthakur/approachingalmost/blob/master/AAAMLP.pdf
+"""
+
+
+def true_positive(y_true, y_pred):
+    """
+    Function to calculate True Positives
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: number of true positives
+    """
+    # initialize
+    tp = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 1 and yp == 1:
+            tp += 1
+    return tp
+
+
+def false_positive(y_true, y_pred):
+    """
+    Function to calculate True Negatives
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: number of true negatives
+    """
+    # initialize
+    fp = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 0 and yp == 1:
+            fp += 1
+    return fp
+
+
+def false_negative(y_true, y_pred):
+    """
+    Function to calculate False Positives
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: number of false positives
+    """
+    # initialize
+    fn = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 1 and yp == 0:
+            fn += 1
+    return fn
+
+
+def true_negative(y_true, y_pred):
+    """
+    Function to calculate False Negatives
+    :param y_true: list of true values
+    :param y_pred: list of predicted values
+    :return: number of false negatives
+    """
+    # initialize
+    tn = 0
+    for yt, yp in zip(y_true, y_pred):
+        if yt == 0 and yp == 0:
+            tn += 1
+    return tn
+
+
+if __name__ == '__main__':
+    y_true = [1, 1, 0, 0, 1, 1, 1, 0, 1]
+    y_pred = [1, 0, 1, 0, 1, 1, 1, 0, 0]
+
+    print('TP:', true_positive(y_true, y_pred))
---
+++ revised_code.py
@@ -0,0 +1,87 @@
+"""
+Alternative implemention of functions for the computation of a
+confusion matrix.
+"""
+
+
+def conf(tars, preds, case):
+    return sum(1 for t, p in zip(tars, preds) if (t, p) == case)
+
+
+def conf_mat(tars, preds, labels=(1, 0)):
+    return [[conf(tars, preds, (t, p)) for t in labels] for p in labels]
+
+
+def example1(tars, preds):
+    # Case constants
+    TP = 1, 1
+    FP = 0, 1
+    FN = 1, 0
+    TN = 0, 0
+
+    print('TP:', conf(tars, preds, TP))
+    print('TN:', conf(tars, preds, TN))
+    print('FP:', conf(tars, preds, FP))
+    print('FN:', conf(tars, preds, FN))
+
+
+def example2(tars, preds):
+    # lambda functions
+    true_positive = lambda tars, preds: conf(tars, preds, (1, 1))
+    true_negative = lambda tars, preds: conf(tars, preds, (0, 0))
+    false_positive = lambda tars, preds: conf(tars, preds, (0, 1))
+    false_negative = lambda tars, preds: conf(tars, preds, (1, 0))
+
+    print('TP:', true_positive(tars, preds))
+    print('TN:', true_negative(tars, preds))
+    print('FP:', false_positive(tars, preds))
+    print('FN:', false_negative(tars, preds))
+
+
+def example3(tars, preds):
+    # partial functions
+    from functools import partial as pf
+    true_positive = pf(conf, case=(1, 1))
+    true_negative = pf(conf, case=(0, 0))
+    false_positive = pf(conf, case=(0, 1))
+    false_negative = pf(conf, case=(1, 0))
+
+    print('TP:', true_positive(tars, preds))
+    print('TN:', true_negative(tars, preds))
+    print('FP:', false_positive(tars, preds))
+    print('FN:', false_negative(tars, preds))
+
+
+def example4():
+    # Strings as class labels
+    tars = 'cat', 'cat', 'dog', 'dog'
+    preds = 'cat', 'dog', 'dog', 'dog'
+    labels = 'cat', 'dog'
+
+    cm = conf_mat(tars, preds, labels)
+    for l, r in zip(labels, cm):
+        print(l, r)
+
+
+def example5():
+    # More than two classes
+    tars = [1, 1, 2, 0, 1, 2, 1, 0, 1]
+    preds = [1, 0, 2, 0, 1, 2, 1, 0, 2]
+    labels = 0,1,2
+
+    for r in conf_mat(tars, preds, labels):
+        print(r)
+
+
+if __name__ == '__main__':
+    tars = [1, 1, 0, 0, 1, 1, 1, 0, 1]
+    preds = [1, 0, 1, 0, 1, 1, 1, 0, 0]
+
+    for r in conf_mat(tars, preds):
+        print(r)
+
+    example1(tars, preds)
+    example2(tars, preds)
+    example3(tars, preds)
+    example4()
+    example5()
